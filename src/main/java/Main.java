35f21318a50a8a988b094b6e8b81281e9810b70d
LuschenkoNM <luschenko@vtb.ru>
2024-07-23 16:04:55 +0300

    add validation path to file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,25 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число:");
-        int firstNumber = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число:");
-        int secondNumber = new Scanner(System.in).nextInt();
-
-        System.out.println("Сумма чисел: " + (firstNumber + secondNumber));
-        System.out.println("Разность чисел: " + (firstNumber - secondNumber));
-        System.out.println("Произведение чисел: " + (firstNumber * secondNumber));
-        System.out.println("Частное чисел: " + ((double) firstNumber / secondNumber));
+        int count = 1;
+        while (true) {
+            System.out.print("Введите путь к файлу: ");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExist = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (!fileExist) {
+                System.out.println("Указанный файл не существует.");
+                continue;
+            }
+            if (isDirectory) {
+                System.out.println("Указанный путь является путём к папке, а не к файлу.");
+                continue;
+            }
+            System.out.println("Путь указан верно. Это файл номер: " + count);
+            count++;
+        }
     }
 }
